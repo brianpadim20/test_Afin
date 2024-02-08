a004c08bfa201e8e4a4a4f035292c25ae02cefa3
Brian Patiño <brianpadim@hotmail.com>
2024-02-08 00:13:02 -0500

    feat: Corrección a la falta de ingesta en la tabla captacion

--- ingesta.py
+++ ingesta.py
@@ -1,33 +1,43 @@
 import os
 import pyodbc
+import re
 
 # Establecer conexión a la base de datos SQL Server
 conexion = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-6QD6LBH;DATABASE=giros_minsalud;Trusted_Connection=yes')
-
+def setfecha():
+        # Expresión regular para buscar el mes y el año en el nombre del archivo
+            patron = r'(\w+)-(\d{4})'
+            coincidencia = re.search(patron, nombre_archivo)
+            if coincidencia:
+                mes = coincidencia.group(1)
+                anio = coincidencia.group(2)
+            else:
+                print("No se pudo encontrar mes y año en el nombre del archivo:", nombre_archivo)
+            
 # Función para insertar datos en la tabla captacion
 def insertar_captacion(datos):
     cursor = conexion.cursor()
     if len(datos) == 9:
-        sql = "INSERT INTO dbo.captacion (MES, ANIO, DANE, DEPARTAMENTO, MUNICIPIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        
-    elif len(datos) == 10:
-        sql = "INSERT INTO dbo.captacion (MES, ANIO, DANE, DEPARTAMENTO, MUNICIPIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, FORMA_DE_CONTRATACIÓN,TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-
+        sql = "INSERT INTO dbo.captacion (DANE, DEPARTAMENTO, MUNICIPIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    elif len(datos) == 10:  # Revisamos la longitud esperada de los datos
+        sql = "INSERT INTO dbo.captacion (DANE, DEPARTAMENTO, MUNICIPIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, COMPLEMENTO_NOMBRE_IPS, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    elif len(datos) == 11:
+        sql = "INSERT INTO dbo.captacion (DANE, DEPARTAMENTO, MUNICIPIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, COMPLEMENTO_NOMBRE_IPS, FORMA_DE_CONTRATACIÓN, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     else:
+        print("Número de campos incorrecto:", datos)
         return
     
     cursor.execute(sql, datos)
     conexion.commit()
 
+
 # Función para insertar datos en la tabla evento
 def insertar_evento(datos):
     cursor = conexion.cursor()
-    if len(datos)== 8:
+    if len(datos) == 8:
         sql = "INSERT INTO dbo.evento (MES, ANIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-    
     elif len(datos) == 9:
         sql = "INSERT INTO dbo.evento (MES, ANIO, COD_EPS, NOMBRE_EPS, NIT_IPS, NOMBRE_IPS, FORMA_DE_CONTRATACIÓN, TOTAL_GIRO, OBSERVACIÓN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    
     else:
         return
     cursor.execute(sql, datos)
@@ -38,7 +48,7 @@ directorio = "csv"
 
 for nombre_archivo in os.listdir(directorio):
     ruta_directorio = os.path.join(directorio, nombre_archivo)
-    
+        
     # Verificar si el nombre del archivo contiene "GIRO DIRECTO EVENTO"
     if "GIRO DIRECTO EVENTO" in nombre_archivo:
         # Obtener mes y año del nombre del archivo
@@ -48,9 +58,6 @@ for nombre_archivo in os.listdir(directorio):
 
         with open(ruta_directorio, "r", encoding="utf-8") as archivo:
             # Leer cada línea del archivo y separar los campos por coma
-            
-            print(f"Ingestando la información del archivo {nombre_archivo} en la tabla evento...")
-            
             for linea in archivo:
                 datos = linea.strip().split(',')
                 # Insertar mes y año en los datos
@@ -58,47 +65,21 @@ for nombre_archivo in os.listdir(directorio):
                 datos.insert(1, anio)
                 # Ingestar los datos en la tabla evento
                 insertar_evento(datos)
-        
         print(f"Datos del archivo {nombre_archivo} ingestado en la tabla evento.")
 
     # Verificar si el nombre del archivo contiene "giro-directo-discriminado-capita-y-evento"
     elif "giro-directo-discriminado-capita-y-evento" in nombre_archivo:
-        # Obtener mes y año del nombre del archivo
-        partes_nombre = nombre_archivo.split(' ')[-1].split('-')
-        mes = partes_nombre[0]
-        anio = partes_nombre[1]
-
+        
         with open(ruta_directorio, "r", encoding="utf-8") as archivo:
             # Leer la primera línea del archivo para determinar si tiene el campo "FORMA DE CONTRATACIÓN"
             primera_linea = archivo.readline()
-            if "FORMA DE CONTRATACIÓN" in primera_linea:
-                # El archivo tiene el campo "FORMA DE CONTRATACIÓN"
-                # Leer cada línea del archivo y separar los campos por coma
-                for linea in archivo:
+            for linea in archivo:
                     datos = linea.strip().split(',')
-                    # Insertar mes y año en los datos
-                    datos.insert(0, mes)
-                    datos.insert(1, anio)
+                    
                     # Ingestar los datos en la tabla captacion
                     insertar_captacion(datos)
                     
-            else:
-                # El archivo no tiene el campo "FORMA DE CONTRATACIÓN"
-                # Regresar al inicio del archivo
-                archivo.seek(0)
-                # Leer cada línea del archivo y separar los campos por coma
-                
-                print(f"Ingestando la información del archivo {datos} en la tabla tabla captación...")
-                
-                for linea in archivo:
-                    datos = linea.strip().split(',')
-                    # Insertar mes y año en los datos
-                    datos.insert(0, mes)
-                    datos.insert(1, anio)
-                    # Ingestar los datos en la tabla captacion
-                    insertar_captacion(datos)
-                    
-        print(f"Datos del archivo {datos} ingestado en la tabla captación.")
+        print(f"Datos del archivo {nombre_archivo} ingestado en la tabla captacion.")
 
     else:
         print("No se encontró un archivo para ingestar")
